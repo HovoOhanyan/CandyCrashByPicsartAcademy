35e81b7a4da6acd23167ecf5cf9386472ad10353
ErBaghdasaryan <baghdasaryan.er@inbox.ru>
2023-09-24 18:36:00 +0400

    addingfirst UI page

---
+++ CandyCrashByPicsartAcademy/UIColor + Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor + Extension.swift
+//  CandyCrashByPicsartAcademy
+//
+//  Created by Er Baghdasaryan on 24.09.23.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    
+    static func gradientFirst1() -> UIColor {
+        return UIColor(red: 1, green: 0.71, blue: 0.867, alpha: 1)
+    }
+    
+    static func gradientFirst2() -> UIColor {
+        return UIColor(red: 0.988, green: 0.396, blue: 0.69, alpha: 1)
+    }
+    
+    static func gradientFirst3() -> UIColor {
+        return UIColor(red: 0.859, green: 0.071, blue: 0.408, alpha: 1)
+    }
+    
+    static func gradientSecond1() -> UIColor {
+        return UIColor(red: 0.412, green: 0.859, blue: 0.992, alpha: 1)
+    }
+    
+    static func gradientSecond2() -> UIColor {
+        return UIColor(red: 0.11, green: 0.769, blue: 0.996, alpha: 1)
+    }
+    
+    static func gradientSecond3() -> UIColor {
+        return UIColor(red: 0, green: 0.439, blue: 0.992, alpha: 1)
+    }
+}
